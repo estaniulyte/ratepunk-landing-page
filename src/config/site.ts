bdd8f1a1411513a1ae2fa531c79289062eba5efe
Evelina Staniulytė <e.staniulyte98@gmail.com>
2023-06-07 19:49:06 +0300

    change: import site logo to config

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,10 +1,12 @@
 import { SiteConfig } from "@/types"
 
+import Logo from '@/assets/logo.svg'
+
 export const siteConfig: SiteConfig = {
   name: "Ratepunk",
   description:
     "Ratepunk compares hotel room prices across the major online travel agencies. When you search for a room, Ratepunk extension scans the top booking sites and runs a price comparison, so you can be confident in knowing you’re getting the best deal!",
-  image: "img",
+  image: Logo,
   links: {
     chromeStore: "https://chrome.google.com/webstore/detail/ratepunk-same-hotel-way-c/gdaioanblcnghddimngklkhgcbomfdck?utm_source=ratepunk",
     appleStore: "https://apps.apple.com/app/ratepunk/id1607823726",
