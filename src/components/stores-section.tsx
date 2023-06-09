ae382053957bc8e1d04c1498dc97d676ec42592a
Evelina Staniulytė <e.staniulyte98@gmail.com>
2023-06-09 16:55:48 +0300

    feat: create stores section

---
+++ src/components/stores-section.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import * as React from "react";
+import Link from "next/link";
+
+import Image from "next/image";
+
+import { siteConfig } from "@/config/site";
+
+import ChromeImage from "@/assets/chrome.svg";
+import AppleImage from "@/assets/apple.svg";
+
+import styles from "@/styles/StoresSection.module.sass";
+
+import { DynamicStar } from "react-dynamic-star";
+
+interface StoresSectionProps {
+  children?: React.ReactNode;
+}
+
+export function StoresSection({ children }: StoresSectionProps) {
+  const items = [
+    {
+      title: "Chrome web Store",
+      href: siteConfig.links.chromeStore,
+      image: ChromeImage,
+    },
+    {
+      title: "Apple app Store",
+      href: siteConfig.links.appleStore,
+      image: AppleImage,
+    },
+  ];
+
+  return (
+    <div className={styles.store}>
+      <div className={styles.store__container}>
+        {items?.map((item, index) => (
+          <Link href={item.href} key={index} className={styles.store__card}>
+            <div>
+              <Image src={item.image} alt="stores" width={60} height={52} />
+            </div>
+            <div>
+              <p>avaliable in the</p>
+              <h2 className={styles.store__card__title}>{item.title}</h2>
+            </div>
+          </Link>
+        ))}
+        <div className={styles.store__reviews}>
+          <DynamicStar className={styles.store__reviews__stars} rating={4.4} outlined={true} width={21} height={21} emptyStarColor="white" />
+          <p>Chrome Stores reviews</p>
+        </div>
+      </div>
+    </div>
+  );
+}
