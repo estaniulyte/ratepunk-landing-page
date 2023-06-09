74f4ed00f85a905e9fa6f2cf1ad77f03eb9de3b9
Evelina Staniulytė <e.staniulyte98@gmail.com>
2023-06-09 22:39:16 +0300

    feat: add data to JSONBIN and add loading state

--- src/components/refer-friends-form.tsx
+++ src/components/refer-friends-form.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 
 import Image from "next/image";
 
@@ -14,24 +14,21 @@ interface ReferFriendsFormProps {
 }
 
 export function ReferFriendsForm({ children }: ReferFriendsFormProps) {
-  const [email, setEmail] = useState<string>("")
-  const [error, setError] = useState<string>("")
+  const [email, setEmail] = useState<string>("");
+  const [error, setError] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
 
-  var emailFormat = /\S+@\S+\.\S+/
+  var emailFormat = /\S+@\S+\.\S+/;
 
   const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     event.preventDefault();
-    setEmail(event.target.value)
-    if (event.target.value == "")
-      setError('Field is required')
-    else if (event.target.value.length < 5)
-      setError('Too short email')
+    setEmail(event.target.value);
+    if (event.target.value == "") setError("Field is required");
+    else if (event.target.value.length < 5) setError("Too short email");
     else if (!emailFormat.test(event.target.value))
-      setError('Email format is incorrect')
-    else if (event.target.value.length > 5)
-      setError('')
-    
-  }
+      setError("Email format is incorrect");
+    else if (event.target.value.length > 5) setError("");
+  };
 
   const [confimedEmail, setConfimedEmail] = useState<boolean>(false);
 
@@ -39,40 +36,96 @@ export function ReferFriendsForm({ children }: ReferFriendsFormProps) {
     return email.length && emailFormat.test(email);
   };
 
+  const onSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    setLoading(true);
+
+    let req = new XMLHttpRequest();
+
+    req.onreadystatechange = () => {
+      if (req.readyState == XMLHttpRequest.DONE) {
+        setConfimedEmail(!confimedEmail);
+        setLoading(false);
+      }
+    };
+
+    req.open(
+      "PUT",
+      "https://api.jsonbin.io/v3/b/648372e78e4aa6225eabc5fa",
+      true
+    );
+    req.setRequestHeader("Content-Type", "application/json");
+    req.setRequestHeader(
+      "X-Master-Key",
+      "$2b$10$/7cn9b9aQTdmJvjoL9ZOqe0e/O3mliHGXtM3sIdIMQcZjbxfYC2.W"
+    );
+    req.send(`{"email": "${email}"}`);
+
+    console.log(req.status);
+  };
+
   return (
     <section className={styles.friends_form}>
       <div>
-        <h2 className={styles.friends_form__title}>REFER FRIENDS AND GET REWARDS</h2>
+        <h2 className={styles.friends_form__title}>
+          REFER FRIENDS AND GET REWARDS
+        </h2>
         <p className={styles.friends_form__description}>
           Refer your friends to us and earn hotel booking vouchers. We'll give
           you 1 coin for each friend that installs our extension. Minimum
           cash-out at 20 coins.
         </p>
-        <form className={styles.friends_form__refferal_form + " " + (confimedEmail ? styles.hidden : " ")}>
-          <span className={styles.friends_form__refferal_form__error}>{error}</span>
+        <form
+          className={
+            styles.friends_form__refferal_form +
+            " " +
+            (confimedEmail ? styles.hidden : " ")
+          }
+          onSubmit={(e) => onSubmit(e)}
+        >
+          <span className={styles.friends_form__refferal_form__error}>
+            {error}
+          </span>
           <input
             className={styles.friends_form__refferal_form__input}
-            type='email'
+            type="email"
             required
             placeholder="Enter your email address"
-            onChange={handleEmailChange} 
+            onChange={handleEmailChange}
           >
             {/* <Image src={EmailIcon} alt="close" width={128} height={140} /> */}
           </input>
           <button
             className={styles.friends_form__refferal_form__button}
-            onClick={() => setConfimedEmail(!confimedEmail)}
-            type='button'
+            type="submit"
             disabled={!validate()}
-          >Get Referral Link</button>
+          >
+            <span className={loading ? styles.hidden : styles.show}>
+              Get Referral Link
+            </span>
+            <span
+              className={
+                styles.loader + " " + (loading ? styles.show : styles.hidden)
+              }
+            ></span>
+          </button>
         </form>
-        <form className={styles.friends_form__email_form + " " + (confimedEmail ? " ": styles.hidden)}>
+        <form
+          className={
+            styles.friends_form__email_form +
+            " " +
+            (confimedEmail ? " " : styles.hidden)
+          }
+        >
           <span className={styles.friends_form__email_form__message}>
             <Image src={SuccessIcon} alt="close" width={24} height={24} />
             Your email is confirmed!
           </span>
           <div className={styles.friends_form__email_form__desktop_input}>
-            <input type='text' defaultValue="https://ratepunk.com/referral"></input>
+            <input
+              type="text"
+              defaultValue="https://ratepunk.com/referral"
+            ></input>
             <button>Copy</button>
           </div>
           <div className={styles.friends_form__email_form__mobile_input}>
@@ -81,7 +134,9 @@ export function ReferFriendsForm({ children }: ReferFriendsFormProps) {
           </div>
         </form>
       </div>
-      <p className={styles.friends_form__limits}>Limits on max rewards apply.</p>
+      <p className={styles.friends_form__limits}>
+        Limits on max rewards apply.
+      </p>
     </section>
   );
 }
