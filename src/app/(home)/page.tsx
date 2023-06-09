9b40be4f44d5724c8e915dfd9c95cc60191e42a2
Evelina Staniulytė <e.staniulyte98@gmail.com>
2023-06-09 13:47:04 +0300

    change: add StoresSection & Footer to main page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,15 @@
 import styles from '@/styles/Home.module.sass'
+
 import { ReferFriendSection } from '@/components/refer-friends-section'
+import { StoresSection } from '@/components/stores-section'
+import { Footer } from '@/components/footer'
 
 export default function Home() {
   return (
     <main>
       <ReferFriendSection />
+      <StoresSection />
+      <Footer />
     </main>
   )
 }
