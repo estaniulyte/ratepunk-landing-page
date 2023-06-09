f4c40168e684a80473638407ffeace9e80595b22
Evelina Staniulytė <e.staniulyte98@gmail.com>
2023-06-09 10:07:46 +0300

    feat: add QuiclLinksConfig

--- src/config/nav.ts
+++ src/config/nav.ts
@@ -1,6 +1,6 @@
-import { NavigationConfig } from "@/types";
+import { MainNavigationConfig, NavigationConfig } from "@/types";
 
-export const navConfig: NavigationConfig = {
+export const navConfig: MainNavigationConfig = {
   mainNav: [
     {
       title: "Chrome Extension",
@@ -13,6 +13,31 @@ export const navConfig: NavigationConfig = {
     {
       title: "Blog",
       href: "/blog",
+    },
+  ],
+};
+
+export const quickLinksConfig: NavigationConfig = {
+  nav: [
+    {
+      title: "Price Comparison",
+      href: "/pricing",
+    },
+    {
+      title: "Chrome Extension",
+      href: "/chrome",
+    },
+    {
+      title: "How It Works",
+      href: "/about",
+    },
+    {
+      title: "Ratepunk Blog",
+      href: "/blog",
+    },
+    {
+      title: "Privacy Policy",
+      href: "/privacy",
     },
   ],
 };
