de381c5c8516af6c25726fadb52c6912e310506f
Evelina Staniulytė <e.staniulyte98@gmail.com>
2023-06-11 22:50:58 +0300

    Update not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,18 +2,12 @@ import styles from "../styles/404.module.sass";
 
 import Image from "next/image";
 
-import NotFoundImage from "@/assets/404.png"
-
+import NotFoundImage from "@/assets/404.png";
 
 export default function Custom404() {
   return (
     <div className={styles.container}>
-      <Image
-        src={NotFoundImage}
-        alt="company logo"
-        width={350}
-        height={350}
-      />
+      <Image src={NotFoundImage} alt="company logo" width={350} height={350} />
       <h1 className={styles.title}>404</h1>
       <p className={styles.description}>
         Oops! The page you're looking for does not exist.
