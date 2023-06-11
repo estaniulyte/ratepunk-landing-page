225e2fcf7dfd27925bccfbde94a6b410851d216f
Evelina Staniulytė <e.staniulyte98@gmail.com>
2023-06-11 22:45:45 +0300

    change: remove styles from import

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,3 @@
-import styles from '@/styles/Home.module.sass'
-
 import { ReferFriendSection } from '@/components/refer-friends-section'
 import { StoresSection } from '@/components/stores-section'
 import { Footer } from '@/components/footer'
