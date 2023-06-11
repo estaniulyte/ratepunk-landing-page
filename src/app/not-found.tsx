92c1cc39a1030c374f3dadaf74c074914915c202
Evelina Staniulytė <e.staniulyte98@gmail.com>
2023-06-11 22:44:54 +0300

    feat: add 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,26 @@
+import styles from "../styles/404.module.sass";
+
+import Image from "next/image";
+
+import NotFoundImage from "@/assets/404.png"
+
+
+export default function Custom404() {
+  return (
+    <div className={styles.container}>
+      <Image
+        src={NotFoundImage}
+        alt="company logo"
+        width={350}
+        height={350}
+      />
+      <h1 className={styles.title}>404</h1>
+      <p className={styles.description}>
+        Oops! The page you're looking for does not exist.
+      </p>
+      <a href="/" className={styles.link}>
+        Go back to the homepage
+      </a>
+    </div>
+  );
+}
